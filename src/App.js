5087a920bc91ee7a97eaff88f2a5cfda51da06b7
Mark Stevens <markspstevens@yahoo.co.uk>
2022-01-26 07:50:13 +0100

    temp under construction page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import data from './assets/data/projectsData.json'
 // import components
 import Header from './components/Header'
 import Home from './pages/Home'
-import About from './pages/About'
+// import About from './pages/About'
+import Construction from './pages/Construction'
 import Project from './pages/Project'
 import Error from './pages/Error'
 import Footer from './components/Footer'
 
 /**
  * Manages routes & renders pages
- * Gets the data (fom file) for the site & passes it to the components
+ * Gets the data (from file) for the site & passes it to the components
  * @function App
  * @returns {array} siteData: all data
  * @returns {JSX}
@@ -23,7 +24,7 @@ const App = () => {
         <Header />   
           <Switch>
             <Route path="/" exact component={()=> <Home siteData={data} />}/>
-            <Route path="/about" exact component={About} />
+            <Route path="/about" exact component={Construction} />
             <Route path="/project/:id" exact component={()=><Project siteData={data} />} />
             <Route component={Error} />
           </Switch> 
